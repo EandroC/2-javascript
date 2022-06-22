dcaf4c36a6ffc8a0f3c1f78ef4e91430353d5409
EandroC <eandrocleiton@gmail.com>
2022-06-22 07:58:51 -0300

    Introdução ao JS: variáveis, condicional, array e loops

---
+++ script.js
@@ -0,0 +1,39 @@
+let idade = 90
+
+let numero1 = '10'
+let numero2 = '20'
+
+let resultado = numero1+'/'+numero2
+
+
+let n1 = 47
+let n2 = 5
+
+let total = n1+n2
+
+console.log("O total foi:" +total)
+
+if(total > 50){
+    console.log("Total foi maior que 50")
+}else{
+    console.log("Total foi menor que 50")
+}
+
+let nome = 'bonieky'
+
+let ingredientes = [
+    '2 xícaras (chá) de açúcar',
+    '3 xícaras (chá) de farinha de trigo',
+    '4 colheres (sopa) de margarina',
+    '3 ovos',
+    '1 e 1/2 xícara (chá) de leite',
+    '1 colher (sopa) bem cheia de fermento em pó'
+];
+
+for(let i = 0;i<ingredientes.length;i++){
+    console.log(ingredientes[i])
+
+}
+
+
+
